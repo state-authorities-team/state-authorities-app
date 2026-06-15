487dc196ccce5136e6c51aededd8032b499b450d
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-06-15 18:48:04 +0300

    fix: add value transforming into number

--- apps/api/src/schemas/news-schema.ts
+++ apps/api/src/schemas/news-schema.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
 export const getNewsQuerySchema = z.object({
-  page: z.int("Must be an intger number").positive("Must be a positive value").default(1),
-  limit: z.int("Must be an integer number").positive("Must be a positive value").default(10),
+  page: z.coerce
+    .number("Must be a number")
+    .int("Must be an integer number")
+    .positive("Must be a positive value")
+    .default(1),
+  limit: z.coerce
+    .number("Must be a number")
+    .int("Must be an integer number")
+    .positive("Must be a positive value")
+    .default(10),
 });
 
 export type GetNewsQuerySchema = z.infer<typeof getNewsQuerySchema>;
