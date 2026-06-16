2b7999c5b65faff2525e70b3864c12c77f967d92
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-06-16 17:29:01 +0300

    feat: make logout endpoint public

--- apps/api/src/routes/auth-routes.ts
+++ apps/api/src/routes/auth-routes.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import * as authController from "../controllers/auth-controller.js";
-import { requireAuth } from "../middlewares/auth-middleware.js";
 import { validateBody } from "../middlewares/validate-body.js";
 import { loginSchema, registrationSchema } from "../schemas/auth-schema.js";
 
@@ -8,5 +7,5 @@ const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registrationSchema), authController.register);
 authRouter.post("/login", validateBody(loginSchema), authController.login);
-authRouter.post("/logout", requireAuth, authController.logout);
+authRouter.post("/logout", authController.logout);
 export default authRouter;
