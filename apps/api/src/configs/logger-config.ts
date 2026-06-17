827bc13284373c0c85fef3a9208082e24fe8b5e1
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-06-17 12:47:39 +0300

    feat: add configuration for winston

---
+++ apps/api/src/configs/logger-config.ts
@@ -0,0 +1,30 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, json, colorize, printf, errors } = format;
+
+const logLevels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const devFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+export const logger = createLogger({
+  levels: logLevels,
+  level: process.env.LOG_LEVEL || (process.env.NODE_ENV === "production" ? "info" : "debug"),
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), errors({ stack: true })),
+  transports: [
+    process.env.NODE_ENV === "production"
+      ? new transports.Console({
+          format: combine(json()),
+        })
+      : new transports.Console({
+          format: combine(colorize({ all: true }), devFormat),
+        }),
+  ],
+});
