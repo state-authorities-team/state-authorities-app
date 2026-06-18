e4556780757763c1a8250a9c113cf4ac2deaab16
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-06-18 09:57:10 +0300

    refactor: create independent express app file

---
+++ apps/api/src/app.ts
@@ -0,0 +1,36 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { setupSwaggerDocs } from "./configs/swagger-config.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import { httpLoggerMiddleware } from "./middlewares/logger-middleware.js";
+import agencyRouter from "./routes/agencies-routes.js";
+import agencyTypeRouter from "./routes/agency-types-routes.js";
+import authRouter from "./routes/auth-routes.js";
+import healthRouter from "./routes/health-routes.js";
+import refreshRouter from "./routes/refresh-routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(httpLoggerMiddleware);
+setupSwaggerDocs(app);
+app.use("/api/agencies", agencyRouter);
+app.use("/api/agency-types", agencyTypeRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/health", healthRouter);
+app.use("/api/refresh", refreshRouter);
+app.use(errorHandler);
+
+export default app;
